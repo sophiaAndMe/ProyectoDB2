850d93fa4837a9cac2140422f4cf351a83660a13
sophiaAndMe <jjlaverde@uce.edu.ec>
2025-02-05 23:14:19 -0500

    first commit

---
+++ src/main/java/ec/edu/uce/ProyectoDB2/ProyectoDb2Application.java
@@ -0,0 +1,13 @@
+package ec.edu.uce.ProyectoDB2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/uce/ProyectoDB2/ProyectoDb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.uce.ProyectoDB2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
