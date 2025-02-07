866c4e3bce3ebb1edf675ec1d9c032039f93bbd7
DiOs01 <dfosorio@uce.edu.ec>
2025-02-06 23:49:53 -0500

    ahi si

--- src/main/java/ec/edu/uce/ProyectoDB2/DataBase/db.js
+++ src/main/java/ec/edu/uce/ProyectoDB2/DataBase/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 // Cluster JHON
-mongoose.connect('mongodb+srv://myAtlasDBUser:<db_password>@myatlasclusteredu.fllri.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU')
+mongoose.connect('mongodb+srv://myAtlasDBUser:g7Thw3BScud9CsLl@myatlasclusteredu.fllri.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU')
     .then(() => console.log('✅ Conectado a MongoDB'))
     .catch(err => console.error('❌ Error al conectar a MongoDB:', err));
 
